666483fda1269c65bff128184fedf0e51a5e0b18
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-18 21:22:14 +0200

    declared variables to convert number to string

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,3 +1,8 @@
 let numero = parseInt(prompt('inserisci un numero di 4 cifre'));
 
+numeroStringa = numero.toString()
+
+arrayNumero = numeroStringa.split("")
 console.log(numero)
+console.log(numeroStringa)
+console.log(numeroStringa)
